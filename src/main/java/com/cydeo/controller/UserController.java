4e002c52492a157f3ab316fe53c88b54e5408b31
fai7h01 <l.varsimashvil10@gmail.com>
2024-04-14 19:19:22 +0400

    user update

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -5,10 +5,7 @@ import com.cydeo.service.RoleService;
 import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/user")
@@ -40,5 +37,12 @@ public class UserController {
         return "redirect:/user/create";
     }
 
+    @GetMapping("/update/{username}")
+    public String editUser(@PathVariable String username, Model model){
+
+        model.addAttribute("user",userService.findById(username));
+
+        return "/user/update";
+    }
 
 }
