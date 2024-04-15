eaeedc34b92633ad920beed9989860b56b50318c
fai7h01 <l.varsimashvil10@gmail.com>
2024-04-15 17:43:26 +0400

    project service

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -1,15 +1,20 @@
 package com.cydeo.controller;
 
+import com.cydeo.dto.ProjectDTO;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("project")
+@RequestMapping("/project")
 public class ProjectController {
 
     @GetMapping("/create")
-    public String create(){
+    public String create(Model model){
+
+        model.addAttribute("project",new ProjectDTO());
+        //model.addAttribute("managers",);
 
         return "/project/create";
     }
