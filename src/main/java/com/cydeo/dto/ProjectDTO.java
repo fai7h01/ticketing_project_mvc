8723b77b72ac795afa5e74acb620785e5c2e5a5c
fai7h01 <l.varsimashvil10@gmail.com>
2024-04-16 16:27:03 +0400

    project service

--- src/main/java/com/cydeo/dto/ProjectDTO.java
+++ src/main/java/com/cydeo/dto/ProjectDTO.java
@@ -4,6 +4,7 @@ import com.cydeo.enums.Status;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
--- src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
@@ -1,13 +1,18 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.ProjectDTO;
+import com.cydeo.enums.Status;
 import com.cydeo.service.ProjectService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class ProjectServiceImpl extends AbstractMapService<ProjectDTO,String> implements ProjectService {
+
     @Override
     public ProjectDTO save(ProjectDTO project) {
+        project.setProjectStatus(Status.OPEN);
         return super.save(project.getProjectCode(),project);
     }
 
