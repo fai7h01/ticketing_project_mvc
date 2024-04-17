861e3cacf3295d7f20be27a891b133833dc89d82
fai7h01 <l.varsimashvil10@gmail.com>
2024-04-17 23:30:27 +0400

    task service

--- src/main/java/com/cydeo/controller/TaskController.java
+++ src/main/java/com/cydeo/controller/TaskController.java
@@ -1,5 +1,9 @@
 package com.cydeo.controller;
 
+import com.cydeo.dto.TaskDTO;
+import com.cydeo.service.ProjectService;
+import com.cydeo.service.TaskService;
+import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,8 +13,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/task")
 public class TaskController {
 
+    private final TaskService taskService;
+    private final UserService userService;
+    private final ProjectService projectService;
+
+    public TaskController(TaskService taskService, UserService userService, ProjectService projectService) {
+        this.taskService = taskService;
+        this.userService = userService;
+        this.projectService = projectService;
+    }
+
     @GetMapping("/create")
     public String createTask(Model model){
+
+        model.addAttribute("task", new TaskDTO());
+        model.addAttribute("projects",projectService.findAll());
+        model.addAttribute("employees",userService.findEmployees());
+        model.addAttribute("tasks",taskService.findAll());
 
         return "/task/create";
     }
--- src/main/java/com/cydeo/dto/TaskDTO.java
+++ src/main/java/com/cydeo/dto/TaskDTO.java
@@ -1,19 +1,26 @@
 package com.cydeo.dto;
 
 import com.cydeo.enums.Status;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TaskDTO {
 
+    private Long Id;
 
     private ProjectDTO project;
-    private UserDTO user;
+    private UserDTO assignedEmployee;
     private String taskSubject;
     private String taskDetail;
+    private Status status;
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate assignedDate;
-    private Status status;
 
 }
