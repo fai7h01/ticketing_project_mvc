3befc965dd2e7b1e38759273fc64b2171700581a
fai7h01 <l.varsimashvil10@gmail.com>
2024-04-22 15:39:35 +0400

    task service

--- src/main/java/com/cydeo/service/TaskService.java
+++ src/main/java/com/cydeo/service/TaskService.java
@@ -2,11 +2,13 @@ package com.cydeo.service;
 
 import com.cydeo.dto.TaskDTO;
 import com.cydeo.dto.UserDTO;
+import com.cydeo.enums.Status;
 
 import java.util.List;
 
 public interface TaskService extends CRUDService<TaskDTO,Long>{
 
     List<TaskDTO> findTaskByManager(UserDTO manager);
+    List<TaskDTO> findAllTasksByStatusIsNot(Status status);
 
 }
