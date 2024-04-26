4c41eb741ca0be358d69322a55dd4df05ee662a0
fai7h01 <l.varsimashvil10@gmail.com>
2024-04-26 16:17:55 +0400

    validation, code clean up

--- src/main/java/com/cydeo/controller/TaskController.java
+++ src/main/java/com/cydeo/controller/TaskController.java
@@ -5,8 +5,10 @@ import com.cydeo.enums.Status;
 import com.cydeo.service.ProjectService;
 import com.cydeo.service.TaskService;
 import com.cydeo.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -25,39 +27,37 @@ public class TaskController {
 
     @GetMapping("/create")
     public String createTask(Model model){
-
         model.addAttribute("task", new TaskDTO());
         model.addAttribute("projects",projectService.findAll());
         model.addAttribute("employees",userService.findEmployees());
         model.addAttribute("tasks",taskService.findAll());
-
         return "/task/create";
     }
 
     @PostMapping("/create")
-    public String insertTask(@ModelAttribute("task") TaskDTO task){
-
+    public String insertTask(@Valid @ModelAttribute("task") TaskDTO task, BindingResult bindingResult, Model model){
+        if (bindingResult.hasErrors()){
+            model.addAttribute("projects",projectService.findAll());
+            model.addAttribute("employees",userService.findEmployees());
+            model.addAttribute("tasks",taskService.findAll());
+            return "/task/create";
+        }
         taskService.save(task);
-
         return "redirect:/task/create";
     }
 
     @GetMapping("/delete/{id}")
     public String deleteTask(@PathVariable("id") Long id){
-
         taskService.deleteById(id);
-
         return "redirect:/task/create";
     }
 
     @GetMapping("/update/{id}")
     public String editTask(@PathVariable("id") Long id, Model model){
-
         model.addAttribute("task",taskService.findById(id));
         model.addAttribute("projects",projectService.findAll());
         model.addAttribute("employees",userService.findEmployees());
         model.addAttribute("tasks",taskService.findAll());
-
         return "/task/update";
     }
 
@@ -71,44 +71,45 @@ public class TaskController {
 //    }
 
     @PostMapping("/update/{id}")
-    public String updateTask(TaskDTO task){
-
+    public String updateTask(@Valid @ModelAttribute("task") TaskDTO task, BindingResult bindingResult, Model model){
+        if (bindingResult.hasErrors()){
+            model.addAttribute("projects",projectService.findAll());
+            model.addAttribute("employees",userService.findEmployees());
+            model.addAttribute("tasks",taskService.findAll());
+            return "/task/update";
+        }
         taskService.update(task);
-
         return "redirect:/task/create";
     }
 
     @GetMapping("/employee/pending-tasks")
     public String employeePendingTasks(Model model){
-
         model.addAttribute("tasks",taskService.findAllTasksByStatusIsNot(Status.COMPLETE));
-
         return "/task/pending-tasks";
     }
 
     @GetMapping("/employee/archive")
     public String employeeArchiveTasks(Model model){
-
         model.addAttribute("tasks",taskService.findAllTasksByStatus(Status.COMPLETE));
-
         return "/task/archive";
     }
 
     @GetMapping("/employee/edit/{id}")
     public String employeeEditTask(@PathVariable("id") Long id, Model model){
-
         model.addAttribute("task",taskService.findById(id));
         model.addAttribute("statuses",Status.values());
         model.addAttribute("tasks",taskService.findAllTasksByStatusIsNot(Status.COMPLETE));
-
         return "/task/status-update";
     }
 
     @PostMapping("/employee/update/{id}")
-    public String employeeUpdateTask(TaskDTO task){
-
+    public String employeeUpdateTask(@Valid @ModelAttribute("task") TaskDTO task, BindingResult bindingResult, Model model){
+        if (bindingResult.hasErrors()){
+            model.addAttribute("statuses",Status.values());
+            model.addAttribute("tasks",taskService.findAllTasksByStatusIsNot(Status.COMPLETE));
+            return "/task/status-update";
+        }
         taskService.updateStatus(task);
-
         return "redirect:/task/employee/pending-tasks";
     }
 
